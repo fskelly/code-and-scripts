ffe706eee937fafe11445e75ae006188e8d7a94e
Fletcher Kelly <fletcher_kelly@outlook.com>
2024-02-29 09:46:53 +0000

    Add tenant selection and subscription retrieval logic***

--- subscriptions/powershell/list_and_connect_to_subscription.ps1
+++ subscriptions/powershell/list_and_connect_to_subscription.ps1
@@ -1,12 +1,36 @@
 try {
   Login-AzAccount
 
-  
+  $tenants = Get-AzTenant | Select-Object Name,Id
+  Write-Output "Found $($tenants.Count) Tenants"
+  $j = 0
+  foreach ($tenant in $tenants)
+  {
+      $tenantValue = $j
+      $tenantText = [string]$tenantValue + " : " + $tenant.Name  + " ( " + $tenant.Id + ")"
+      Write-Output $tenantText
+      $j++
+  }
+  Do 
+  {
+      [int]$tenantChoice = Read-Host -Prompt "Select tenant number & press enter"
+  } 
+  until ($tenantChoice -le $tenants.Count)
 
+  $selectedTenant = "You selected Tenant ID: " + $tenants[$tenantChoice].Name + " ( " + $tenants[$tenantChoice].Id + " ) "
+  Write-Output $selectedTenant
+  Connect-AzAccount -Tenant $tenants[$tenantChoice].Id
 
-  $subscriptions = Get-AzSubscription | Sort-Object SubscriptionName | Select-Object Name,SubscriptionId
+  Write-output "Getting subscriptions that yo have access to from $($tenants[$tenantChoice].Name) tenant"
+  $subscriptions = Get-AzSubscription -TenantId $tenants[$tenantChoice].Id | Sort-Object SubscriptionName | Select-Object Name,SubscriptionId
   [int]$subscriptionCount = $subscriptions.count
-  Write-output "Found" $subscriptionCount "Subscriptions"
+
+  if ($subscriptionCount -eq 0) {
+    Write-Output "No subscriptions found for this tenant."
+    exit
+  }
+
+  Write-output "Found $subscriptionCount Subscriptions"
   $i = 0
   foreach ($subscription in $subscriptions)
   {
